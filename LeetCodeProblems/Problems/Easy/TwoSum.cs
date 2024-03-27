e669c940cc94066e0fca8b32ce02499c64f406f4
PavelKhrynov <DarkPreacher@live.ru>
2024-03-27 21:17:26 +0400

    TwoSum Problem

---
+++ LeetCodeProblems/Problems/Easy/TwoSum.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems.Problems.Easy
+{
+    /*
+        Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+        You may assume that each input would have exactly one solution, and you may not use the same element twice.
+        You can return the answer in any order.
+    */
+    internal class TwoSum
+    {
+        public static int[] SolveProblem(int[] nums, int target)
+        {
+            var numHashtable = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                numHashtable[nums[i]] = i;
+            }
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int firstNum = nums[i];
+                int secondNum = target - firstNum;
+                if (numHashtable.TryGetValue(secondNum, out int secondNumPos) && secondNumPos != i)
+                {
+                    return new int[] { i, numHashtable[secondNum] };
+                }
+            }
+
+            return new int[0];
+        }
+    }
+}
--- LeetCodeProblems/Program.cs
+++ LeetCodeProblems/Program.cs
@@ -1,10 +1,16 @@
-﻿namespace LeetCodeProblems
+﻿using LeetCodeProblems.Problems.Easy;
+
+namespace LeetCodeProblems
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] nums = new int[] { 3, 2, 4 };
+            int target = 6;
+
+            int[] result = TwoSum.SolveProblem(nums, target);
+            Console.WriteLine($"[{string.Join(", ", result)}]");
         }
     }
 }
