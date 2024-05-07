d0d9b440a9cb9a72aab7ee4b275a16b6110ea9ed
Pavel Khrynov <darkpreacher@live.ru>
2024-05-07 12:57:51 +0300

    RemoveDuplicatesFromSortedArray Problem

---
+++ LeetCodeProblems/Problems/Easy/RemoveDuplicatesFromSortedArray.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems.Problems.Easy
+{
+    /*
+     * Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. 
+     * The relative order of the elements should be kept the same. 
+     * Then return the number of unique elements in nums.
+     * Consider the number of unique elements of nums to be k, to get accepted, you need to do the following things:
+     * - Change the array nums such that the first k elements of nums contain the unique elements in the order they were present in nums initially.
+     *   The remaining elements of nums are not important as well as the size of nums.
+     * - Return k.
+     */
+    internal class RemoveDuplicatesFromSortedArray
+    {
+        public static int SolveProblem(int[] nums)
+        {
+            int posPointer = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (i + 1 >= nums.Length || nums[i + 1] > nums[i])
+                {
+                    nums[posPointer] = nums[i];
+                    posPointer++;
+                }
+            }
+
+            return posPointer;
+        }
+    }
+}
--- LeetCodeProblems/Program.cs
+++ LeetCodeProblems/Program.cs
@@ -7,13 +7,13 @@ namespace LeetCodeProblems
     {
         static void Main(string[] args)
         {
-            int[] array = new int[] { 3, 2, 2, 1 };
+            int[] array = new int[] { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
             int value = 3;
             int randomValue = new Random().Next(9999);
             string stringNum1 = "123";
             string stringNum2 = "456";
 
-            string result = MultiplyStrings.SolveProblem(stringNum1, stringNum2);
+            int result = RemoveDuplicatesFromSortedArray.SolveProblem(array);
             Console.WriteLine(result);
             //Console.WriteLine($"[{string.Join(", ", result)}]");
         }
